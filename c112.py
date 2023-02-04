0a1c4f9e32f05e80ac41ca84865161e74b346758
DhruvSharmaCoder <96577282+DhruvSharmaCoder@users.noreply.github.com>
2023-02-04 20:10:56 +0530

    Add files via upload

---
+++ c112.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+from_dir = 'c:/users/dell/downloads'
+to_dir = 'c:/users/dell/downloads/Telegram Desktop'
+
+list_of_files = os.listdir(from_dir)
+#print(list_of_files)
+for file_name in list_of_files:
+    name,extension = os.path.splitext(file_name)
+   # print(name)
+    print(extension)
+    if extension == '':
+        continue
+
+    if extension in [ '.jpg','.JPG','.pdf','.PDF','.jpeg','.JPEG']:
+        print('inif')
+        path1 = from_dir+'/'+file_name 
+        path2 = to_dir+'/'+'image_files'
+        path3 = to_dir+'/'+'image_files'+'/'+file_name
+        print(path1)
+        print(path3) 
+        
+        if os.path.exists(path2):
+           print('moving'+file_name)
+           shutil.move(path1,path3)
+        else:
+           os.makedirs(path2)
+           print('moving'+file_name)
+           shutil.move(path1,path3)
+
+print('hi')
